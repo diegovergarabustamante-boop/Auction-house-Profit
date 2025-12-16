41fa1c0abe875dfe09d3b7fa90f3c84ff78d5d44
dvergara123 <diegovergarabustamante@gmail.com>
2025-12-16 20:37:28 -0300

    base datos 2

--- wow-marketplace/backend/market/admin.py
+++ wow-marketplace/backend/market/admin.py
@@ -5,12 +5,72 @@ from .models import (
     Material,
     ItemMaterial,
     ConnectedRealm,
-    ItemPriceSnapshot
+    PriceSnapshot,
+    ArbitrageResult,
 )
 
-admin.site.register(Profession)
-admin.site.register(Item)
-admin.site.register(Material)
-admin.site.register(ItemMaterial)
-admin.site.register(ConnectedRealm)
-admin.site.register(ItemPriceSnapshot)
+# -------------------------
+# Básicos
+# -------------------------
+
+@admin.register(Profession)
+class ProfessionAdmin(admin.ModelAdmin):
+    search_fields = ("name",)
+
+
+@admin.register(Material)
+class MaterialAdmin(admin.ModelAdmin):
+    search_fields = ("name",)
+
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ("name", "blizzard_id")
+    search_fields = ("name",)
+    
+
+@admin.register(ItemMaterial)
+class ItemMaterialAdmin(admin.ModelAdmin):
+    list_display = ("item", "material", "quantity_required")
+    list_filter = ("item", "material")
+
+
+@admin.register(ConnectedRealm)
+class ConnectedRealmAdmin(admin.ModelAdmin):
+    list_display = ("blizzard_id", "name", "slug")
+    search_fields = ("name", "slug")
+
+
+# -------------------------
+# Precios por reino
+# -------------------------
+
+@admin.register(PriceSnapshot)
+class PriceSnapshotAdmin(admin.ModelAdmin):
+    list_display = (
+        "item",
+        "realm",
+        "min_price",
+        "created_at",
+    )
+    list_filter = ("realm",)
+    search_fields = ("item__name",)
+    date_hierarchy = "created_at"
+
+
+# -------------------------
+# Resultados de arbitrage
+# -------------------------
+
+@admin.register(ArbitrageResult)
+class ArbitrageResultAdmin(admin.ModelAdmin):
+    list_display = (
+        "item",
+        "buy_realm",
+        "sell_realm",
+        "profit",
+        "created_at",
+    )
+    list_filter = ("buy_realm", "sell_realm")
+    search_fields = ("item__name",)
+    date_hierarchy = "created_at"
